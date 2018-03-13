922aed9336964234c0b9952df0587adebd734d35
Jana Kantorová <jkantorova@blueberryapps.com>
2018-03-13 09:20:15 +0100

    add-navbar-change-color-on-scroll

--- src/homepage/navbar.js
+++ src/homepage/navbar.js
@@ -1,12 +1,13 @@
 
 
-
-$(window).scroll(function(){
-  var scroll = $(window).scrollTop();
-  if(scroll > 20){
-    $(".navbar").addClass("navbar--scrolled");
-  } else {
-    $(".navbar").removeClass("navbar--scrolled");
-  }
+$(document).ready(function(){
+	$(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+    if(scroll > 20){
+    	$(".navbar").css("background", "grey");
+    }
+    else{
+    	$(".navbar").css("background", "none");
+    }
+  });
 });
-
